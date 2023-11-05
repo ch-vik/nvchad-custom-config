be6e64f930e3388b874d3f602437106adfa556be
Kevin C <kevin.ceresa@gmail.com>
2023-11-05 21:41:14 +0100

    [markdown] Add prettier format on save

--- plugins.lua
+++ plugins.lua
@@ -210,6 +210,12 @@ local plugins = {
         }
       }
     end
+  },
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function() vim.fn["mkdp#util#install"]() end,
   }
 }
 
